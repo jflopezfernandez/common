c81784b57e5a2505267859c720be036e87119550
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2019-09-01 19:21:41 -0400

    Added documentation for the struct settings_t object

--- include/settings.h
+++ include/settings.h
@@ -2,6 +2,15 @@
 #ifndef PROJECT_INCLUDES_SETTINGS_H
 #define PROJECT_INCLUDES_SETTINGS_H
 
+/** There are two command-line options at the moment that affect the operation
+ *  of the application. The first controls the verbosity of the output during
+ *  program execution. It has been implemented in a limited capacity so far,
+ *  but the option itself is fully operational. The threads setting controls
+ *  how many threads the program creates to operate on the input files. At the
+ *  moment, the number of threads is expected to be even to be cleanly divided
+ *  up into the two input files, and if it isn't, it is incremented by one.
+ * 
+ */
 struct settings_t {
     int verbose;
     int threads;
