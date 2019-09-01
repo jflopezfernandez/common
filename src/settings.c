5dd2d42db3ebe06b4fa343ab2dafb983ee69ec14
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2019-09-01 19:24:52 -0400

    Added documentation for the settings static object

--- src/settings.c
+++ src/settings.c
@@ -1,6 +1,11 @@
 
 #include "common.h"
 
+/** There is a single settings object created during the lifetime of the
+ *  application and it is here; the settings object itself has internal linkage
+ *  and is accessed via the getter and setter functions.
+ * 
+ */
 static struct settings_t settings;
 
 void settings_set_verbose(int setting) {
